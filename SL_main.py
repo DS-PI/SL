43f4fdfa1dbe53edef1a1d815ba0818d4b7c343d
DS-PI <121065596+DS-PI@users.noreply.github.com>
2023-01-12 14:09:47 +0700

    Update SL_main.py

--- SL_main.py
+++ SL_main.py
@@ -6,8 +6,7 @@ import pandas as pd  # pip install pandas openpyxl
 import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 import streamlit_authenticator as stauth  # pip install streamlit-authenticator
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
+
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Stock Calculation Dashboard", page_icon=":bar_chart:", layout="wide")
